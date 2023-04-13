fca1b8fe86b1559bcb6811d07bbd0c07e72a8634
Людмила Алтухова <luda.tamb@mail.ru>
2023-04-13 14:20:12 +0300

    refactor: add global styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createGlobalStyle } from "styled-components";
 
 import App from "./App";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Montserrat', sans-serif;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  button {
+    cursor: pointer;
+    border: none;
+    background: none;
+  }
+`;
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
