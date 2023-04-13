3a2ed3d44ed42e15e7ac0fb11a9e261336d78d11
Людмила Алтухова <luda.tamb@mail.ru>
2023-04-13 14:38:08 +0300

    feat: implement product list component

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,7 +1,25 @@
+import styled from "styled-components";
+
 import Product from "./Product";
+import { products } from "../data/products";
+
+const Products = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  gap: 20px;
+`;
 
 const ProductList = () => {
-  return <ul></ul>;
+  return (
+    <Products>
+      {products.map((item, key) => (
+        <li key={item.id}>
+          <Product item={item} />
+        </li>
+      ))}
+    </Products>
+  );
 };
 
 export default ProductList;
