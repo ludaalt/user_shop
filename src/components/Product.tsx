aa03901bfae0e1889fb1b524296be01cb3814219
Людмила Алтухова <luda.tamb@mail.ru>
2023-04-14 16:33:27 +0300

    feat: implement product card component

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,5 +1,36 @@
+import styled from "styled-components";
+
+import Button from "./Button";
+
+const ProductItem = styled.div`
+  border: 3px solid pink;
+  padding: 10px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  border: 1px solid #e1e1e4;
+  border-radius: 20px;
+
+  text-transform: lowercase;
+  font-size: 17px;
+  font-weight: bold;
+
+  :hover {
+    transform: scale(1.01);
+  }
+`;
+
 const Product = ({ item }: any) => {
-  return <li>{item.title}</li>;
+  return (
+    <ProductItem>
+      <img src={item.image} alt={item.title} height={300} />
+      {item.title}
+      <br />
+      Brand: {item.brand}
+      <Button>Купить</Button>
+    </ProductItem>
+  );
 };
 
 export default Product;
