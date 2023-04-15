661fb601245fe37ddc981a9ad53d1fef77a9b007
Людмила Алтухова <luda.tamb@mail.ru>
2023-04-15 15:40:28 +0300

    feat: implement changing product count

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 import Button from "./Button";
+
+const StyledButton = styled(Button)<{ countInCart: number }>`
+  margin: 10px 0;
+
+  width: ${(props) => (props.countInCart === 0 ? `100%` : `auto`)};
+  max-width: ${(props) => (props.countInCart === 0 ? `auto` : `65px`)};
+  cursor: ${(props) => (props.countInCart !== 0 ? `auto` : `pointer`)};
+`;
 
 const ProductItem = styled.div`
   border: 3px solid pink;
@@ -15,20 +24,61 @@ const ProductItem = styled.div`
   text-transform: lowercase;
   font-size: 17px;
   font-weight: bold;
+`;
 
-  :hover {
-    transform: scale(1.01);
-  }
+const ChangeCountButton = styled.button<{ countInCart: number }>`
+  position: absolute;
+  margin-top: auto;
+  margin-bottom: auto;
+  top: 0;
+  bottom: 0;
+  text-align: center;
+  padding: 0 2px;
+
+  display: ${(props) => (props.countInCart === 0 ? `none` : `block`)};
 `;
 
 const Product = ({ item }: any) => {
+  const [countInCart, setCountInCart] = useState<number>(0);
+
   return (
     <ProductItem>
       <img src={item.image} alt={item.title} height={300} />
       {item.title}
       <br />
       Brand: {item.brand}
-      <Button>Купить</Button>
+      <div
+        style={{
+          minWidth: "110px",
+          position: "relative",
+          padding: 0,
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <ChangeCountButton
+          style={{ left: "0" }}
+          countInCart={countInCart}
+          onClick={() => setCountInCart((prev) => prev + 1)}
+        >
+          +1
+        </ChangeCountButton>
+        <StyledButton
+          countInCart={countInCart}
+          onClick={() =>
+            countInCart === 0 ? setCountInCart((prev) => prev + 1) : null
+          }
+        >
+          {countInCart === 0 ? `Купить` : countInCart}
+        </StyledButton>
+        <ChangeCountButton
+          style={{ right: "0" }}
+          countInCart={countInCart}
+          onClick={() => setCountInCart((prev) => prev - 1)}
+        >
+          -1
+        </ChangeCountButton>
+      </div>
     </ProductItem>
   );
 };
