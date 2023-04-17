755eecc2bf9c343547ab225857f9d30845da2a4a
Людмила Алтухова <luda.tamb@mail.ru>
2023-04-17 11:05:53 +0300

    refactor: correct main component

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,10 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import styled from "styled-components";
 
 import Aside from "../components/Aside";
 import ProductList from "../components/ProductList";
 import { products } from "../data/products";
+import { IProductItem } from "../types/types";
 
 const AppContainer = styled.div`
   padding: 20px 5px;
@@ -13,39 +14,30 @@ const AppContainer = styled.div`
 `;
 
 const App = () => {
-  const [isFiltered, setIsFiltered] = useState(false);
-  const [filteredBrands, setFilteredBrands] = useState<any>([]);
+  const [filteredBrands, setFilteredBrands] = useState<string[]>([]);
 
-  console.log(isFiltered);
-
-  const [visibleProducts, setVisibleProducts] = useState<any>(products);
+  const [visibleProducts, setVisibleProducts] =
+    useState<IProductItem[]>(products);
 
   const showFiltered = () => {
-    // console.log(999);
-    if (filteredBrands.length > 0) {
-      // console.log(111);
-      setVisibleProducts((prev: any) =>
-        products.filter((item: any) => filteredBrands.includes(item.brand))
-      );
-    }
-    if (filteredBrands.length === 0) {
-      // console.log(333);
-      setVisibleProducts(products);
-    }
+    setVisibleProducts(
+      products.filter((item: IProductItem) =>
+        filteredBrands.includes(item.brand.toString())
+      )
+    );
   };
 
-  useEffect(() => {
-    showFiltered();
-  }, [isFiltered]);
+  const showProducts = () => {
+    setVisibleProducts(products);
+    setFilteredBrands((a: any) => a.splice(0, a.length));
+  };
 
   return (
     <AppContainer>
-      <Aside {...{ setFilteredBrands, setIsFiltered, showFiltered }} />
-      <ProductList
-        filteredBrands={filteredBrands}
-        isFiltered={isFiltered}
-        visibleProducts={visibleProducts}
+      <Aside
+        {...{ setFilteredBrands, showFiltered, showProducts, filteredBrands }}
       />
+      <ProductList visibleProducts={visibleProducts} />
     </AppContainer>
   );
 };
