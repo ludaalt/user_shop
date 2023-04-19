ea61ca9742541a69cd19e9b02e0cd5ec866684b6
Людмила Алтухова <luda.tamb@mail.ru>
2023-04-19 20:24:38 +0300

    feat: implement app context

--- src/context/context.tsx
+++ src/context/context.tsx
@@ -1,18 +1,57 @@
-import React, { FC, useState } from "react";
+import React, { FC, useState, useEffect } from "react";
 
-import { ProductsContextType, IProductItem } from "../types/types";
+import {
+  IProductItem,
+  IProductItemInCart,
+  InitProductsContextType,
+} from "../types/types";
 
-export const ProductsContext = React.createContext<ProductsContextType | null>(
-  null
-);
+export const ProductsContext = React.createContext<InitProductsContextType>({});
 
 interface Props {
-  children?: React.ReactNode | any;
+  children?: React.ReactNode;
 }
 
 const ProductsProvider: FC<Props> = ({ children }) => {
+  const [productsInCart, setProductsInCart] = useState<IProductItemInCart[]>(
+    []
+  );
+  const [totalPrice, setTotalPrice] = useState<number>(0);
+
+  const buyProduct = (
+    product: IProductItem | IProductItemInCart,
+    count: number
+  ) => {
+    const productInCart = productsInCart.find((item) => item.id === product.id);
+
+    if (productInCart) {
+      setProductsInCart((prev) =>
+        prev.map((item) =>
+          item.id === product.id ? { ...item, count: item.count + count } : item
+        )
+      );
+    } else {
+      setProductsInCart((prev) => [...prev, { ...product, count: count }]);
+    }
+  };
+
+  useEffect(() => {
+    setTotalPrice(
+      +productsInCart
+        .reduce(
+          (acc, item) => acc + item["regular_price"]["value"] * item.count,
+          0
+        )
+        .toFixed(2)
+    );
+  }, [productsInCart]);
+
   return (
-    <ProductsContext.Provider value={{}}>{children}</ProductsContext.Provider>
+    <ProductsContext.Provider
+      value={{ buyProduct, productsInCart, totalPrice }}
+    >
+      {children}
+    </ProductsContext.Provider>
   );
 };
 
